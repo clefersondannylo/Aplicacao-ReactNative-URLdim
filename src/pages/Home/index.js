aaf53456a1166bb9f2b2c18875dea3f9aa5410bc
clefersondannylo <clefersonnascimento8@gmail.com>
2021-06-02 15:15:43 -0300

    adicionar o dismiss do keyboard

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,9 @@
 import React from "react";
+import {
+  TouchableWithoutFeedback,
+  Keyboard,
+  KeyboardAvoidingView,
+} from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 import StatusBarPage from "../../components/StatusBarPage";
 import Menu from "../../components/Menu";
@@ -12,36 +17,44 @@ import {
   BoxIcon,
   Input,
   ButtonLink,
-  ButtonLinkText
+  ButtonLinkText,
 } from "./styles";
 import { Feather } from "@expo/vector-icons";
-
 export default function Home() {
   return (
-    <LinearGradient
-      colors={["#0085ff", "#132742"]}
-      style={{ flex: 1, justifyContent: "center" }}
-    >
-      <StatusBarPage barStyle="ligh-content" backgroundColor="#0085ff" />
-      <Menu />
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <LinearGradient
+        colors={["#0085ff", "#132742"]}
+        style={{ flex: 1, justifyContent: "center" }}
+      >
+        <StatusBarPage barStyle="ligh-content" backgroundColor="#0085ff" />
+        <Menu />
+        <KeyboardAvoidingView>
+          <ContainerLogo>
+            <Logo
+              source={require("../../assets/Logo.png")}
+              resizeMode="contain"
+            />
+          </ContainerLogo>
+          <ContainerContent>
+            <Title>URLdim</Title>
+            <SubTitle>Cole o link que deseja encurtar</SubTitle>
 
-      <ContainerLogo>
-        <Logo source={require("../../assets/Logo.png")} resizeMode="contain" />
-      </ContainerLogo>
-      <ContainerContent>
-        <Title>URLdim</Title>
-        <SubTitle>Cole o link que deseja encurtar</SubTitle>
-
-        <ContainerInput>
-          <BoxIcon>
-            <Feather name="link" size={22} color="#fff" />
-          </BoxIcon>
-          <Input placeholder="Cole seu link aqui..." placeholderTextColor="#fff"/>
-        </ContainerInput>
-        <ButtonLink>
-          <ButtonLinkText>Gerar Link</ButtonLinkText>
-        </ButtonLink>
-      </ContainerContent>
-    </LinearGradient>
+            <ContainerInput>
+              <BoxIcon>
+                <Feather name="link" size={22} color="#fff" />
+              </BoxIcon>
+              <Input
+                placeholder="Cole seu link aqui..."
+                placeholderTextColor="#fff"
+              />
+            </ContainerInput>
+            <ButtonLink>
+              <ButtonLinkText>Gerar Link</ButtonLinkText>
+            </ButtonLink>
+          </ContainerContent>
+        </KeyboardAvoidingView>
+      </LinearGradient>
+    </TouchableWithoutFeedback>
   );
 }
