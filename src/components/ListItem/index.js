8101c73b646a22328b507f397194ccc993c369a8
clefersondannylo <clefersonnascimento8@gmail.com>
2021-06-03 17:28:12 -0300

    adicionar lista de links

---
+++ src/components/ListItem/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {View, Text} from 'react-native'
+import {} from './styles'
+
+export default function ListItem(){
+    return(
+        <View>
+            <Text>https://seulink.com</Text>
+        </View>
+    )
+}
--- src/pages/MyLinks/index.js
+++ src/pages/MyLinks/index.js
@@ -1,10 +1,23 @@
 import React from "react";
 import { View, Text } from "react-native";
+import { Container, Title, ListLinks } from "./styles";
+import StatusBarPage from "../../components/StatusBarPage";
+import Menu from "../../components/Menu";
+import ListItem from '../../components/ListItem'
 
 export default function MyLinks() {
   return (
-    <View>
-      <Text>Página MyLinks </Text>
-    </View>
+    <Container>
+      <StatusBarPage barStyle="light-content" backgroundColor="#132742" />
+      <Menu />
+      <Title>Meus links </Title>
+      <ListLinks
+        data={[{ id: 1, link: "test.com" },{ id: 2, link: "test.com" }]}
+        keyExtractor={(item) => String(item.id)}
+        renderItem={({item})=> <ListItem data={item}/>}
+        contentContainerStyle={{paddingBottom:20}}
+        showsVerticalScrollIndicator={false}
+      />
+    </Container>
   );
 }
